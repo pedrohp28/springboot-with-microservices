203b7ccc5ed793264a799b4565ed3f46f0435f39
pedrohp28 <pedrohp36@gmail.com>
2025-07-09 13:33:04 -0300

    Iniciando projeto

---
+++ springboot-with-microservices/src/main/java/br/com/pedrohp28/springboot_with_microservices/Startup.java
@@ -0,0 +1,13 @@
+package br.com.pedrohp28.springboot_with_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ springboot-with-microservices/src/test/java/br/com/pedrohp28/springboot_with_microservices/Startup.java
@@ -0,0 +1,13 @@
+package br.com.pedrohp28.springboot_with_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startup {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
